416ff80391026e10197dd6ad6552d29d3329125b
abdelbge2023 <123804406+abdelbge2023@users.noreply.github.com>
2025-10-29 17:39:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ class SamarcheApp {
           apiKey: "AIzaSyDkqudvQPUv_Lh2V2d2PUSEcxcHDExw6PE",
           authDomain: "gestion-fermebenamara.firebaseapp.com",
           projectId: "gestion-fermebenamara",
-          storageBucket: "gestion-fermebenamara.firebasestorage.app",
+          storageBucket: "gestion-fermebenamara.appspot.com",
           messagingSenderId: "668129137491",
           appId: "1:668129137491:web:b56522302ea789044507a6"
         };
@@ -593,6 +593,7 @@ let app;
 document.addEventListener('DOMContentLoaded', () => {
     app = new SamarcheApp();
 });
+
 
 
 
