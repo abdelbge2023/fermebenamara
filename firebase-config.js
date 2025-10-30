57a3591e43f5d2be6d55463b8d90fbb94676e281
abdelbge2023 <123804406+abdelbge2023@users.noreply.github.com>
2025-10-30 23:12:12 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,23 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-if (typeof firebase !== 'undefined') {
-  const firebaseConfig = {
-    apiKey: "AIzaSyDkqudvQPUv_Lh2V2d2PUSEcxcHDExw6PE",
-    authDomain: "gestion-fermebenamara.firebaseapp.com",
-    projectId: "gestion-fermebenamara",
-    storageBucket: "gestion-fermebenamara.firebasestorage.app",
-    messagingSenderId: "668129137491",
-    appId: "1:668129137491:web:b56522302ea789044507a6"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDkqudvQPUv_Lh2V2d2PUSEcxcHDExw6PE",
+  authDomain: "gestion-fermebenamara.firebaseapp.com",
+  projectId: "gestion-fermebenamara",
+  storageBucket: "gestion-fermebenamara.firebasestorage.app",
+  messagingSenderId: "668129137491",
+  appId: "1:668129137491:web:b56522302ea789044507a6"
+};
 
 // Initialize Firebase
- if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig);
-        console.log('✅ Firebase initialisé avec succès');
-    } else {
-        console.log('✅ Firebase déjà initialisé');
-    }
-} else {
-    console.warn('❌ Firebase non disponible');
-}
+const app = initializeApp(firebaseConfig);
