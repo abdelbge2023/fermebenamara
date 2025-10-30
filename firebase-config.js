fc97c505eb17fe43babd5df59cf2b798a0affcae
abdelbge2023 <123804406+abdelbge2023@users.noreply.github.com>
2025-10-30 23:13:50 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDkqudvQPUv_Lh2V2d2PUSEcxcHDExw6PE",
-  authDomain: "gestion-fermebenamara.firebaseapp.com",
-  projectId: "gestion-fermebenamara",
-  storageBucket: "gestion-fermebenamara.firebasestorage.app",
-  messagingSenderId: "668129137491",
-  appId: "1:668129137491:web:b56522302ea789044507a6"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDkqudvQPUv_Lh2V2d2PUSEcxcHDExw6PE",
+    authDomain: "gestion-fermebenamara.firebaseapp.com",
+    projectId: "gestion-fermebenamara",
+    storageBucket: "gestion-fermebenamara.firebasestorage.app",
+    messagingSenderId: "668129137491",
+    appId: "1:668129137491:web:b56522302ea789044507a6"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
