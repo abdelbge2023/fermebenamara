b2c54b3742329749a03cbc52ba446f17cff4129f
abdelbge2023 <123804406+abdelbge2023@users.noreply.github.com>
2025-10-30 20:09:09 +0100

    Create migration.js

---
+++ migration.js
@@ -0,0 +1,10 @@
+// migration.js - Fichier de secours
+export async function migrerVersFirebase() {
+    console.log('Migration vers Firebase - Fonction non disponible');
+    alert('Firebase n\\'est pas configuré. Utilisation du localStorage.');
+}
+
+export async function verifierDonneesFirebase() {
+    console.log('Vérification Firebase - Fonction non disponible');
+    alert('Firebase n\\'est pas configuré.');
+}
